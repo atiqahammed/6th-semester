9491e5dd34b8d400d4cf5bdd18f2574ab77bd8cd
atiqahammed <atiqahammedshamim@gmail.com>
2018-10-12 12:43:07 +0600

    driver is working now

--- 605-ST/lab/Selenium/src/googleTest/GoogleTest.java
+++ 605-ST/lab/Selenium/src/googleTest/GoogleTest.java
@@ -1,11 +1,17 @@
 package googleTest;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleTest {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\atiqa\\Downloads\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+		System.out.println(driver.getTitle());
 
 	}
 
