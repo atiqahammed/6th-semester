02dd592a813fdd6a2a71a3e2e393a391a9d51d7f
atiqahammed <atiqahammedshamim@gmail.com>
2018-10-12 12:40:45 +0600

    working on chrome driver

---
+++ 605-ST/lab/Selenium/src/googleTest/GoogleTest.java
@@ -0,0 +1,12 @@
+package googleTest;
+
+import org.openqa.selenium.WebDriver;
+
+public class GoogleTest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+
+	}
+
+}
