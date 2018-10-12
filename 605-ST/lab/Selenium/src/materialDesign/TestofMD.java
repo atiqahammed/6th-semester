17250efd788a586d0b3fa23361671f35b50db1ff
atiqahammed <atiqahammedshamim@gmail.com>
2018-10-12 14:24:01 +0600

    travelled to foundation page

--- 605-ST/lab/Selenium/src/materialDesign/TestofMD.java
+++ 605-ST/lab/Selenium/src/materialDesign/TestofMD.java
@@ -12,18 +12,34 @@ public class TestofMD {
 
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
-		
-		//System.out.println(driver.getTitle());
-
 		WebElement searchBox = driver.findElement(By.id("lst-ib"));
 		searchBox.sendKeys("material design");
 		searchBox.submit();
 		
-		//driver.findElement(By.xpath("//a[@href ='/docs/configuration']")).click();
+		//search completed
+		
 		
 		driver.findElement(By.xpath("//*[@id='rso']/div[1]/div/div[1]/div/div/div[1]/a[1]")).click();
 		
-		//*[@id="rso"]/div[1]/div/div[1]/div/div/div[1]/a[1]
+		driver.findElement(By.xpath("/html/body/header/button[1]/i")).click();
+		
+	//	 /html/body/header/button[1]/i
+		
+		
+		
+		driver.findElement(By.xpath("/html/body/nav/ul[2]/li[3]/button")).click();
+		driver.findElement(By.xpath("//a[@href ='./foundation-overview']")).click();
+		
+		//href="./layout/understanding-layout.html"
+		
+		
+		//driver.findElement(By.xpath("//*[@id='_nav4']/ul/li[1]/a")).click();
+		
+		
+		//*[@id="_nav6"]/ul/li[1]/a
+		
+		// /html/body/nav/ul[2]/li[5]/button
+		
 		
 		
 		
